45f0c2d92643a603819ed7fcafc222dff803cb9a
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-08-01 15:00:43 +0700

    Player Game

--- Othello/Player.cs
+++ Othello/Player.cs
@@ -1,15 +1,12 @@
+using InterfacePlayer;
 namespace Player;
-public class IPlayer
-{
-    public int IdPlayer {get;}
-    public string Username {get;}
-}
-public class Player{
-    public int Id {get; private set;}
-    public string Username {get; private set;}
-    public Player(int idPlayer, string username) 
-    {
-        this.Id = idPlayer;
-        this.Username = username;
-    }
+
+public class Player: IPlayer{
+	public int IdPlayer {get; private set;}
+	public string Username {get; private set;}
+	public Player(int idPlayer, string username) 
+	{
+		this.IdPlayer = idPlayer;
+		this.Username = username;
+	}
 }
