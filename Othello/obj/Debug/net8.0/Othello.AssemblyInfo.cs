e829853af17586be98860cb7a5b3ecdfb002d111
Nabilah Hanun <hanunnabilah3@gmail.com>
2024-08-01 15:01:21 +0700

    Update

--- Othello/obj/Debug/net8.0/Othello.AssemblyInfo.cs
+++ Othello/obj/Debug/net8.0/Othello.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Othello")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d55bd6b9bf35ff65bfc4ebad85aab087fce5125")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cc989095c495df187c160076388172cc49af3d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Othello")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Othello")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
